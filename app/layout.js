fdfbc72aafe69d7d7742791e18b722b305684204
Sergio Nezhigay <abc1971h@gmail.com>
2023-08-26 17:19:13 +0300

    no meta

--- app/layout.js
+++ app/layout.js
@@ -11,37 +11,37 @@ export default function Layout({ children }) {
   );
 }
 
-export const metadata = {
-  title: 'CarpTravel',
-  description:
-    'We offer you unforgettable trips to the most beautiful parts of the Carpathians. Enjoy stunning views, exciting expeditions, and the best service!',
-  openGraph: {
-    title: 'Best travel site',
-    description:
-      'We offer you unforgettable trips to the most beautiful parts of the Carpathians. Enjoy stunning views, exciting expeditions, and the best service!',
-    url: process.env.BASE_URL,
-    siteName: 'CarpTravel',
-    images: [
-      {
-        url: '/images/hero-bg-768.jpg',
-        width: 768,
-        height: 530,
-      },
-    ],
-    locale: 'en_US',
-    type: 'website',
-  },
-  robots: {
-    index: false,
-    follow: true,
-    nocache: true,
-    googleBot: {
-      index: true,
-      follow: false,
-      noimageindex: true,
-      'max-video-preview': -1,
-      'max-image-preview': 'large',
-      'max-snippet': -1,
-    },
-  },
-};
+// export const metadata = {
+//   title: 'CarpTravel',
+//   description:
+//     'We offer you unforgettable trips to the most beautiful parts of the Carpathians. Enjoy stunning views, exciting expeditions, and the best service!',
+//   openGraph: {
+//     title: 'Best travel site',
+//     description:
+//       'We offer you unforgettable trips to the most beautiful parts of the Carpathians. Enjoy stunning views, exciting expeditions, and the best service!',
+//     url: process.env.BASE_URL,
+//     siteName: 'CarpTravel',
+//     images: [
+//       {
+//         url: '/images/hero-bg-768.jpg',
+//         width: 768,
+//         height: 530,
+//       },
+//     ],
+//     locale: 'en_US',
+//     type: 'website',
+//   },
+//   robots: {
+//     index: false,
+//     follow: true,
+//     nocache: true,
+//     googleBot: {
+//       index: true,
+//       follow: false,
+//       noimageindex: true,
+//       'max-video-preview': -1,
+//       'max-image-preview': 'large',
+//       'max-snippet': -1,
+//     },
+//   },
+// };
