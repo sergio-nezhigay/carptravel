3efece9792de53289f4c91aef7358d0c4202ab7c
Sergio Nezhigay <abc1971h@gmail.com>
2023-08-26 17:15:45 +0300

    return meta

--- app/layout.js
+++ app/layout.js
@@ -1,49 +1,47 @@
 import { Inter } from 'next/font/google';
 
+const inter = Inter({ subsets: ['latin'] });
 import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] });
-
-// export const metadata = {
-//   title: 'CarpTravel',
-//   'next-size-adjust': 'test',
-//   description:
-//     'We offer you unforgettable trips to the most beautiful parts of the Carpathians. Enjoy stunning views, exciting expeditions, and the best service!',
-//   openGraph: {
-//     title: 'Best travel site',
-//     description:
-//       'We offer you unforgettable trips to the most beautiful parts of the Carpathians. Enjoy stunning views, exciting expeditions, and the best service!',
-//     url: process.env.BASE_URL,
-//     siteName: 'CarpTravel',
-//     images: [
-//       {
-//         url: '/images/hero-bg-768.jpg',
-//         width: 768,
-//         height: 530,
-//       },
-//     ],
-//     locale: 'en_US',
-//     type: 'website',
-//   },
-//   robots: {
-//     index: false,
-//     follow: true,
-//     nocache: true,
-//     googleBot: {
-//       index: true,
-//       follow: false,
-//       noimageindex: true,
-//       'max-video-preview': -1,
-//       'max-image-preview': 'large',
-//       'max-snippet': -1,
-//     },
-//   },
-// };
-
-export default function RootLayout({ children }) {
+export default function Layout({ children }) {
   return (
-    <html lang="en" suppressHydrationWarning={true}>
+    <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
   );
 }
+
+export const metadata = {
+  title: 'CarpTravel',
+  description:
+    'We offer you unforgettable trips to the most beautiful parts of the Carpathians. Enjoy stunning views, exciting expeditions, and the best service!',
+  openGraph: {
+    title: 'Best travel site',
+    description:
+      'We offer you unforgettable trips to the most beautiful parts of the Carpathians. Enjoy stunning views, exciting expeditions, and the best service!',
+    url: process.env.BASE_URL,
+    siteName: 'CarpTravel',
+    images: [
+      {
+        url: '/images/hero-bg-768.jpg',
+        width: 768,
+        height: 530,
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  robots: {
+    index: false,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: false,
+      noimageindex: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+};
