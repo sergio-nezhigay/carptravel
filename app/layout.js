ee3844c9c8dad01b8d681afd0f3006309d8ae2a3
Sergio Nezhigay <abc1971h@gmail.com>
2023-10-02 14:35:55 +0300

    add meta

--- app/layout.js
+++ app/layout.js
@@ -13,38 +13,29 @@ export default function Layout({ children }) {
   );
 }
 
+const BASE_URL = 'https://carptravelling.vercel.app/';
+const TITLE = 'CarpTravel';
+const DESCRIPTION =
+  'We offer you unforgettable trips to the most beautiful parts of the Carpathians. Enjoy stunning views, exciting expeditions, and the best service!';
+
 export const metadata = {
-  title: 'CarpTravel',
-  description:
-    'We offer you unforgettable trips to the most beautiful parts of the Carpathians. Enjoy stunning views, exciting expeditions, and the best service!',
+  title: TITLE,
+  description: DESCRIPTION,
   openGraph: {
-    title: 'Best travel site',
-    description:
-      'We offer you unforgettable trips to the most beautiful parts of the Carpathians. Enjoy stunning views, exciting expeditions, and the best service!',
+    title: TITLE,
+    description: DESCRIPTION,
     url: '/',
-    siteName: 'CarpTravel',
-    images: [
-      {
-        url: '/images/hero-bg-768.jpg',
-        width: 768,
-        height: 530,
-      },
-    ],
-    locale: 'en_US',
+    siteName: TITLE,
+    locale: 'uk',
     type: 'website',
   },
-  // metadataBase: new URL('https://acme.com'),
-  robots: {
-    index: false,
-    follow: true,
-    nocache: true,
-    googleBot: {
-      index: true,
-      follow: false,
-      noimageindex: true,
-      'max-video-preview': -1,
-      'max-image-preview': 'large',
-      'max-snippet': -1,
-    },
+  metadataBase: new URL(BASE_URL),
+  manifest: '/meta/manifest.webmanifest',
+  robots: 'all',
+  twitter: {
+    card: 'summary_large_image',
+  },
+  alternates: {
+    canonical: BASE_URL,
   },
 };
